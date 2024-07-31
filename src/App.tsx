8455ed65746ff0a1d477124194e753de82d0324f
Gabriel San Martin <gabisanmartin12@gmail.com>
2024-07-31 16:07:58 -0300

    feat: configure ReactQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <main>
-      <h1>Ice & fire</h1>
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main>
+        <h1>Ice & fire</h1>
+      </main>
+    </QueryClientProvider>
   );
 }
 
