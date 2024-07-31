5dfb4f70e434dae8e1fd5374d9cad2ec4407790c
Gabriel San Martin <gabisanmartin12@gmail.com>
2024-07-31 16:42:48 -0300

    fix: useHouses.

--- src/hooks/IceAndFire/houses/useHouses.ts
+++ src/hooks/IceAndFire/houses/useHouses.ts
@@ -5,7 +5,7 @@ import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 type Output = Awaited<ReturnType<typeof services.iceAndFire.houses.getHouses>>;
 
 export const useHouses = <T = Output>(
-  options?: Omit<UseQueryOptions<Output, Error, T>, "queryKey" | "queryFn">
+  options: Omit<UseQueryOptions<Output, Error, T>, "queryKey" | "queryFn"> = {}
 ) =>
   useQuery<Output, Error, T>({
     ...options,
